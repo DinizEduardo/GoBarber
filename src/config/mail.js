23eb8d54cc5c59ecd57b4f67ad5895ba70298e36
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-06-29 20:18:16 -0300

    Envio de email (Modulo 03)

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '99040016fbf875',
+    pass: '820b3f0336c905',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

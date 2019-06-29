5d89783ac3038bf36116e3a77a59df6ef13e1736
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-06-29 16:17:15 -0300

    Agendamento do usuario (Modulo 03)

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // A tabela de appointments tem relacionamento com a mesma tabela duas vezes
+    // O Usuario que marcou o appointment e o prestador que vai realizar o serviço
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;

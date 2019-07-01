046211a66a038fe63b0fc3fb5331c3fdaf61e4bc
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-01 17:17:23 -0300

    Variaveis de ambiente usando dotenv (Fim Modulo 03)

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '7d0e4dfca02d6c0b629f2298a15e1484',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '99040016fbf875',
-    pass: '820b3f0336c905',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',

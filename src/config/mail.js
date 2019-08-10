91c9ecbbd6d67d3fc86bf12cb98370689687b696
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-08-10 14:19:20 -0300

    Alterações para fazer o app e o site

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,9 @@
 export default {
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: false,
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: 'e9cb7a3614d6bf',
+    pass: '8c3d7b4fc50754',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
